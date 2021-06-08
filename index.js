28ae1982f9185c7083525855f4c26026449a6474
hidwoc <heidischoi@gmail.com>
2021-06-08 16:03:48 -0400

    Adjust nutritional value to return 2 decimal places Change INGREDIENTS section to details and summary element

--- index.js
+++ index.js
@@ -16,17 +16,26 @@ const recipeBox = document.querySelector("#recipe-box");
 // // make sure this opens to blank webpage
 // // append anchor element to h4
 // // create img element and assign its src to recipes.recipe.image
-// // create section element and assign it recipes.recipe.ingredientLines
+// // create details element and assign it recipes.recipe.ingredientLines
+// // create summary element, set innerText = "Ingredients"
+// // append summary element to details element
 // // // oof, this might be overkill. May have to switch tactics to focus on health/dietLabels
-// // // handle .quantity to have only 2 decimal places!
 // // create p element with class .energy
-// // assign innerText = `Energy: ${recipes.recipe.totalNutrients.ENERC_KCAL.quantity} ${recipes.recipe.totalNutrients.ENERC_KCAL.unit}`
+// // const bigE = recipes.recipe.totalNutrients.ENERC_KCAL.quantity
+// // const smallE = bigE.toFixed(2)
+// // assign innerText = `Energy: ${smallE} ${recipes.recipe.totalNutrients.ENERC_KCAL.unit}`
 // // create p element with class .protein
-// // assign innerText = `Protein: ${recipes.recipe.totalNutrients.PROCNT.quantity} ${recipes.recipe.totalNutrients.PROCNT.unit}`
+// // const bigP = recipes.recipe.totalNutrients.PROCNT.quantity
+// // const smallP = bigP.toFixed(2)
+// // assign innerText = `Protein: ${smallP} ${recipes.recipe.totalNutrients.PROCNT.unit}`
 // // create p element with class .fat
-// // assign innerText = `Fat: ${recipes.recipe.totalNutrients.FAT.quantity} ${recipes.recipe.totalNutrients.FAT.unit}`
+// // const bigF = recipes.recipe.totalNutrients.FAT.quantity
+// // const smallF = bigF.toFixed(2)
+// // assign innerText = `Fat: ${smallF} ${recipes.recipe.totalNutrients.FAT.unit}`
 // // create p element with class .carbs
-// // assign innerText = `Carbs: ${recipes.recipe.totalNutrients.CHOCDF.quantity} ${recipes.recipe.totalNutrients.CHOCDF.unit}`
+// // const bigC = recipes.recipe.totalNutrients.CHOCDF.quantity
+// // const smallC = bigC.toFixed(2)
+// // assign innerText = `Carbs: ${smallC} ${recipes.recipe.totalNutrients.CHOCDF.unit}`
 // // // might also consider totalDaily for percentage of daily intake rather than straight numbers from totalNutrients???
 // // // in which case I should definitely calculate for a SINGLE SERVING
 // // append elements to .recipeCard
