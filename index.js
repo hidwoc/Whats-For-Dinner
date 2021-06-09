48326510b4fd64b7484186d8951585e1a186ddfd
hidwoc <heidischoi@gmail.com>
2021-06-09 12:49:20 -0400

    Successful API fetch '

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ const findRecipeCards = (event) => {
   const ingredientSearch = ingredientInput.value;
   // take #ingredients-input values to search API for results
   const url = `https://api.edamam.com/search?q=${ingredientSearch}&app_id=610593b2&app_key=c3fe8717d94ed5f74e0c0bce360bb3ca`
-  axios.get(ur)
+  axios.get(url)
     .then((res) => {
       // declare variable recipes = results.data.hits
       const recipes = res.data.hits;
-      // recipes.forEach((recipe) => createRecipeCard(recipe))
-      recipes.forEach((recipe) => createRecipeCard(recipe))
+      console.log(recipes);
+      recipes.forEach((i) => createRecipeCard(i))
+   
     }
     )
   // // // account for filter options - OR TOGGLE RESULTS???
@@ -23,43 +24,75 @@ const findRecipeCards = (event) => {
 
 
 // create function to createRecipeCard (to be used in .forEach())
-const createRecipeCard = (recipe) => {
+const createRecipeCard = (i) => {
   // // create div element with class .recipeCard
   const recipeCard = document.createElement('div');
   recipeCard.className = 'recipeCard';
-  // // create h4 element and assign it recipes.recipe.label
+
+  // // create h4 element and assign it i.recipe.label
   const label = document.createElement('h4');
-  label.value = recipes.recipe.label;
+  label.value = i.recipe.label;
   // // // account for "recipe" in label and remove?
-  // // create anchor element and assign its href to recipes.recipe.url
-  
+  // // create anchor element and assign its href to i.recipe.url
+  const labelLink = document.createElement('a');
+  labelLink.href = i.recipe.url
   // // make sure this opens to blank webpage
+  labelLink.setAttribute("target", "_blank");
   // // append anchor element to h4
-  // // create img element and assign its src to recipes.recipe.image
-  // // create details element and assign it recipes.recipe.ingredientLines
+  label.appendChild(labelLink);
+
+  // // create img element and assign its src to i.recipe.image
+  const recipeImg = document.createElement('img');
+  recipeImg.src = i.recipe.image;
+
+  // // create details element and set innerText to i.recipe.ingredientLines
+  const recipeIngredients = document.createElement('details');
+  recipeIngredients.innerText = i.recipe.ingredientLines;
   // // create summary element, set innerText = "Ingredients"
+  const recipeIngredientsSummary = document.createElement('summary');
+  recipeIngredientsSummary.innerText = "Ingredients";
   // // append summary element to details element
+  recipeIngredients.appendChild(recipeIngredientsSummary);
   // // // oof, this might be overkill. May have to switch tactics to focus on health/dietLabels
+  
   // // create p element with class .energy
-  // // const bigE = recipes.recipe.totalNutrients.ENERC_KCAL.quantity
-  // // const smallE = bigE.toFixed(2)
-  // // assign innerText = `Energy: ${smallE} ${recipes.recipe.totalNutrients.ENERC_KCAL.unit}`
+  const energy = document.createElement("p");
+  energy.className = "energy";
+  // // assign innerText = `Energy: ${smallE} ${i.recipe.totalNutrients.ENERC_KCAL.unit}`
+  const bigE = i.recipe.totalNutrients.ENERC_KCAL.quantity;
+  const smallE = bigE.toFixed(2);
+  energy.innerText = `Energy: ${smallE} ${i.recipe.totalNutrients.ENERC_KCAL.unit}`;
+  
   // // create p element with class .protein
-  // // const bigP = recipes.recipe.totalNutrients.PROCNT.quantity
-  // // const smallP = bigP.toFixed(2)
-  // // assign innerText = `Protein: ${smallP} ${recipes.recipe.totalNutrients.PROCNT.unit}`
+  const protein = document.createElement("p");
+  protein.className = "protein";
+  // // assign innerText = `Protein: ${smallP} ${i.recipe.totalNutrients.PROCNT.unit}`
+  const bigP = i.recipe.totalNutrients.PROCNT.quantity;
+  const smallP = bigP.toFixed(2);
+  protein.innerText = `Protein: ${smallP} ${i.recipe.totalNutrients.PROCNT.unit}`;
+  
   // // create p element with class .fat
-  // // const bigF = recipes.recipe.totalNutrients.FAT.quantity
-  // // const smallF = bigF.toFixed(2)
-  // // assign innerText = `Fat: ${smallF} ${recipes.recipe.totalNutrients.FAT.unit}`
+  const fat = document.createElement("p");
+  fat.className = "fat";
+  // // assign innerText = `Fat: ${smallF} ${i.recipe.totalNutrients.FAT.unit}`
+  const bigF = i.recipe.totalNutrients.FAT.quantity;
+  const smallF = bigF.toFixed(2);
+  fat.innerText = `Fat: ${smallF} ${i.recipe.totalNutrients.FAT.unit}`;
+  
   // // create p element with class .carbs
-  // // const bigC = recipes.recipe.totalNutrients.CHOCDF.quantity
-  // // const smallC = bigC.toFixed(2)
-  // // assign innerText = `Carbs: ${smallC} ${recipes.recipe.totalNutrients.CHOCDF.unit}`
+  const carbs = document.createElement("p");
+  carbs.className = "carbs";
+  // // assign innerText = `Carbs: ${smallC} ${i.recipe.totalNutrients.CHOCDF.unit}`
+  const bigC = i.recipe.totalNutrients.CHOCDF.quantity;
+  const smallC = bigC.toFixed(2);
+  carbs.innerText = `Carbs: ${smallC} ${i.recipe.totalNutrients.CHOCDF.unit}`;
   // // // might also consider totalDaily for percentage of daily intake rather than straight numbers from totalNutrients???
   // // // in which case I should definitely calculate for a SINGLE SERVING
-  // // append elements to .recipeCard
+  
+  // // append elements to recipeCard
+  recipeCard.append(label,recipeImg,recipeIngredients,energy,protein,fat,carbs);
   // // append .recipeCard to recipeBox
+  recipeBox.appendChild(recipeCard);
 }
   
 // addEventListener on 'submit' to #submit to findRecipeCards
